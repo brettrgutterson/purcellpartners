73752b8e7b2659d1dd29409fbfd07c2e8904cff3
Brett Gutterson <BrettRobertGutterson@outlook.com.au>
2025-07-27 15:19:41 +1000

    Add interfaces and implementations to allow splitting out of functionality

--- src/PurcellPartners/Program.cs
+++ src/PurcellPartners/Program.cs
@@ -1,11 +1,16 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
 using PurcellPartners.Common.Application;
 using PurcellPartners.Common.ConfigurationSetting;
+using PurcellPartners.Common.InputHandling;
+using PurcellPartners.Common.OutputHandling;
 
 var services = new ServiceCollection();
 
 services.AddSingleton<ConfigurationSettingManager>();
 services.AddTransient<IApplication, Application>();
+services.AddSingleton<IOutputHandler, OutputHandler>();
+services.AddSingleton<IInputHandler, InputHandler>();
+services.AddSingleton<IOutputHandler,  OutputHandler>();
 
 var serviceProvider = services.BuildServiceProvider();
 
