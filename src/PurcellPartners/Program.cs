b2b05d2326b822f8e90d42853822c0d215849b55
Brett Gutterson <BrettRobertGutterson@outlook.com.au>
2025-07-27 12:20:28 +1000

    Add application orchestration class and DI it into Program.cs

--- src/PurcellPartners/Program.cs
+++ src/PurcellPartners/Program.cs
@@ -1,1 +1,12 @@
-﻿Console.ReadLine();
+﻿using Microsoft.Extensions.DependencyInjection;
+using PurcellPartners.Common.Application;
+
+var services = new ServiceCollection();
+services.AddTransient<IApplication, Application>();
+var serviceProvider = services.BuildServiceProvider();
+
+var app = serviceProvider.GetRequiredService<IApplication>();
+bool result = app.TestMethod();
+Console.WriteLine($"TestMethod returned: {result}");
+
+Console.ReadLine();
